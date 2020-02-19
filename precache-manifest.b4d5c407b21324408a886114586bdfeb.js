89aa3811aed95e61098fdfd01103a61e1da6e35b
Tiago Azevedo <tiago.azevedo@promptlyhealth.com>
2020-02-19 20:22:20 +0000

    Updates

---
+++ precache-manifest.b4d5c407b21324408a886114586bdfeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d939e7223e0cf3b6faddc0ba24f229b6",
+    "url": "/simple-calculator/index.html"
+  },
+  {
+    "revision": "259e9497d94bbc171dbf",
+    "url": "/simple-calculator/static/css/main.513b4203.chunk.css"
+  },
+  {
+    "revision": "8aac8fd2284bb454aab2",
+    "url": "/simple-calculator/static/js/2.17eec309.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/simple-calculator/static/js/2.17eec309.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "259e9497d94bbc171dbf",
+    "url": "/simple-calculator/static/js/main.394230b0.chunk.js"
+  },
+  {
+    "revision": "b82fd816b6d849d7a823",
+    "url": "/simple-calculator/static/js/runtime-main.b0062540.js"
+  }
+]);
